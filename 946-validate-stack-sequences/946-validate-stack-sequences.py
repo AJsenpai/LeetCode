fcfb97c8628fe7f1ee77a4a787a7c9399af6bc90
Ajay <ajayidc007@gmail.com>
2022-01-07 16:48:54 +0530

    Time: 72 ms (66.05%), Space: 14.4 MB (60.37%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.py
+++ 946-validate-stack-sequences/946-validate-stack-sequences.py
@@ -1,5 +1,12 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+    # 1. Keep pushing pushed elements into stack if the top element
+    #    on the stack is different from the current one of popped;
+    # 2. Keep poping out of the top element from stack if it is same 
+    #    as the current one of popped;
+    # 3. Check if the stack is empty after loop.
+    
+        # O(N) T | O(N) S
         stack,index = [],0
         for val in pushed:
             stack.append(val)
