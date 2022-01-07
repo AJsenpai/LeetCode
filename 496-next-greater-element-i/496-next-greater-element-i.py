984bf73974e794c16491623237cfd5d2d022d626
Ajay <ajayidc007@gmail.com>
2022-01-07 23:32:16 +0530

    Time: 48 ms (75.29%), Space: 14.4 MB (74.55%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,37 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        '''
+        1. build hashmap to store number: its next bigger
+        2. loop: 
+            use stack; so long the stack has value and stack.top < current number
+            hashmap[stack.pop()] = current_number
+           stack.push(current_numer)
+         
+         3. at last stack will only has number which doesnt have any next_bigger
+         4. iterate over nums1 and check if the value is present in hash
+            if yes; then pop its next bigger else add -1 
+            
+            O(n) T | O(1) S
+        '''
+        mapping,stack = {},[]        
+        # build hashmap with key= number, val = nextbigger 
+        for x in nums2:
+            while stack and stack[-1]< x:
+                # for all these numbers x is the next bigger
+                mapping[stack.pop()] = x
+            stack.append(x)
+        
+        result = []
+        
+        for x in nums1:
+            if x in mapping:
+                result.append(mapping[x])
+            else:
+                result.append(-1)
+        return result
+            
+        
+            
+            
+            
+        
