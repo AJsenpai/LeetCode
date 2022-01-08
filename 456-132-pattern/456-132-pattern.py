a3f217547d3e45fccaef196cba99f92d3f145fcf
Ajay <ajayidc007@gmail.com>
2022-01-09 00:02:16 +0530

    Time: 392 ms (42.90%), Space: 37.2 MB (5.49%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,38 +1,60 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        ## RC ##
-        ## APPROACH : STACK ##
-        ## LOGIC ##
-        ## 1. We Create Minimum Array, till that position => O(n)
-        ## 2. We start iterating from reverse of given array.
-        ## 3. Remember we are using Stack and TopOfStack to determine, 2 in 132 pattern. ( so we have to check)
-        ## 4. At any position, we push all elements IF greater than minimum (possible 2 in 132 pattern)
-        ## 5. At any position, we pop all stack elements IF topOfStack is less or EQUAL to minimum (invalid element to form 132 pattern)
-        ## 6. SATISFYING CONDITION : at any stage if min_nums[i] < stack[-1] < nums[i] we return True.
+        ## Logic ##
+        ## Brute force - use 3 loops for i,j,k and compare -> O(N^3) T
         
-		## TIME COMPLEXITY : O(N) ##
-		## SPACE COMPLEXITY : O(N) ##
+        ## Optimal Solution
+        ## condition - i<k<j
         
-        ## EDGE CASE : While checking the conditions 4,5,6. We should perform pop operation first i.e remove invalid elements before inserting the current element into the stack.
-        
-        if len(set(nums)) < 3:
+        ## 1. create a minimum array (min_so_far) this will be our i
+        ## 2. start iterating reverse in array (this is for j)
+        ## 3. we will use stack and stack.top to determine k (2 in 132 pattern)
+        ## 4. At any position we push element if it is greater then minimum 
+        ##    element (possible 2 in 132 patter since 2>1)
+        ## 5. At any position, we check if stack.top is less than or equal to min
+        ##    element; if so we pop the elements using loop (invalid element for 132)
+        ## 6. satisfying condition: at any stage if 
+        ##    min_so_far[i] < stack[-1] < nums [i] 
+        ##        i             k            j
+        ## Edge Case: before checking 4,6 pop the invalid elements first before 
+        ##            inserting new current element
+        ## O(N) T | O(N) S
+            
+        if len(set(nums))<3:
             return False
-
+        
+        # 1
         min_nums = [nums[0]]
         for i in range(1, len(nums)):
-            min_nums.append(min(nums[i], min_nums[-1]))
-            
-        stack = []  
-        i = len(nums) - 1
-        for i in range(len(nums)-1, -1, -1):
+            min_nums.append(min(min_nums[-1], nums[i]))
+        
+        stack = []
+        i= len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
             # 4
-            if( nums[i] > min_nums[i] ):
+            if nums[i] > min_nums[i]:
                 # 5
-                while( stack and stack[-1] <= min_nums[i] ):
+                while stack and stack[-1] <= min_nums[i]:
                     stack.pop()
-                # 6
-                if(stack and min_nums[i] < stack[-1] < nums[i] ):
+                
+                #6
+                if stack and min_nums[i] < stack[-1] < nums[i]:
                     return True
-                # 4
+                
+                #4
                 stack.append(nums[i])
-        return False    
+        
+        return False
+                
+            
+        
+        
+                
+        
+        
+        
+        
+        
+        
+   
+
