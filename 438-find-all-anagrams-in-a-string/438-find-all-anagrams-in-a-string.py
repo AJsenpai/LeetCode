e4b1d6dd7537aa81e02c045a550c373e6e084823
Ajay <ajayidc007@gmail.com>
2022-01-15 18:11:48 +0530

    Time: 2552 ms (10.13%), Space: 15.2 MB (41.62%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        start,end = 0,0
+        result = []
+        word_count = {}
+        match = 0
+        for char in p:
+            if char not in word_count:
+                word_count[char] = 0
+            word_count[char] += 1
+        
+        while end<len(s):
+            
+            # calculation
+            right_char = s[end]
+            if right_char in word_count:
+                word_count[right_char] -= 1            
+                if word_count[right_char] == 0:
+                    match += 1
+            
+            
+            if (end-start+1) == len(p):
+                # answer
+                if match == len(set(p)):
+                    result.append(start)
+                
+                left_char = s[start]
+                if left_char in word_count:
+                    if word_count[left_char] == 0:
+                        match -= 1
+                    
+                    word_count[left_char] += 1
+                start +=1
+            end +=1
+        return result
+                
