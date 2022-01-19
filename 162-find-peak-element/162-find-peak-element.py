adf5e88bb9c2df206d4be4fcc6c214f1957efde9
Ajay <ajayidc007@gmail.com>
2022-01-19 22:10:43 +0530

    Time: 54 ms (39.33%), Space: 14.6 MB (11.07%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -6,8 +6,7 @@ class Solution:
         start,end = 0,len(a)-1
         while start<=end:
             # left | mid | right
-            mid = start + (end-start)//2
-            
+            mid = start + (end-start)//2          
             if mid > 0 and mid<len(a)-1:
                 if a[mid] > a[mid-1] and a[mid] > a[mid+1]:
                     return mid
@@ -16,17 +15,20 @@ class Solution:
                 elif a[mid+1] > a[mid]:
                     start = mid +1
             else:
+                # edge case
                 if mid==0:
                     if a[0] > a[1]:
                         return 0
                     else:
                         return 1
                 
+                
                 elif mid==len(a)-1:
-                    if a[len(a)-1] > a[len(a)-2]:
-                        return len(a)-1
+                    n = len(a)
+                    if a[n-1] > a[n-2]:
+                        return n-1
                     else:
-                        return len(a)-2
+                        return n-2
                         
                     
             
