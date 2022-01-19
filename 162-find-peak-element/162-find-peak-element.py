a02e9cf7453dd472fba8c2a7630ce2e0baf65b2d
Ajay <ajayidc007@gmail.com>
2022-01-19 22:20:28 +0530

    Time: 59 ms (34.53%), Space: 14.4 MB (71.71%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -6,20 +6,25 @@ class Solution:
         start,end = 0,len(a)-1
         while start<=end:
             # left | mid | right
-            mid = start + (end - start) // 2
-            left = a[mid - 1] if mid > 0 else float('-inf')
-            right = a[mid + 1] if mid < len(a) - 1 else float('-inf')
+            mid = start + (end-start)//2
+            left = a[mid-1] if mid > 0 else float('-inf')
+            right = a[mid+1] if mid < len(a)-1 else float('-inf')
 
-            # print(mid,left,right)
-            if left < a[mid] and right > a[mid]:  # right greater
+            if left > a[mid] and right < a[mid]: # left greater go left
+                end = mid - 1 
+
+            elif left < a[mid] and right > a[mid]: # right greater go right
                 start = mid + 1
-            elif left > a[mid] and right < a[mid]:  # left greater
-                end = mid - 1
-            elif left > a[mid] and right > a[mid]:  # both greater - go either way
-                end = mid - 1
-            else:
+
+            elif left > a[mid] and right > a[mid]: # both greater go either way
+                start = mid +1 
+
+            else: # a[mid] > left and  a[mid] > right 
                 return mid
+
         return -1
+    
+# another code    
 #             mid = start + (end-start)//2          
 #             if mid > 0 and mid<len(a)-1:
 #                 if a[mid] > a[mid-1] and a[mid] > a[mid+1]:
