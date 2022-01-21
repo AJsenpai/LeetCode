b2e6a54d96053b09cda9f0db4d627d9ac4d514e0
Ajay <ajayidc007@gmail.com>
2022-01-21 17:20:54 +0530

    Time: 56 ms (41.62%), Space: 14.3 MB (7.34%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -10,19 +10,18 @@ class Solution:
         
         while start<=end:
             mid = start + (end-start)//2            
-            
             square = (mid*mid)
-            ceil_square = ((mid+1)*(mid+1))
             
-            if square <= x and ceil_square > x:
-                return mid
-            
-            elif  square > x:
+            if  square > x:
                 end = mid - 1
             
-            elif square < x:                            
+            elif square < x:
+                ceil_square = ((mid+1)*(mid+1))
+                if ceil_square > x: # edge case/ we need floor
+                    return mid
                 start = mid +1
-            
+            else:
+                return mid
         return -1
     
                     
