7f4989eba8aa5a0f56b187c4ace700ca313040bf
Ajay <ajayidc007@gmail.com>
2022-02-23 22:18:06 +0530

    Time: 116 ms (89.01%), Space: 14.1 MB (52.22%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,27 +1,33 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        # variable size window
         start,end = 0,0
-        char_count = {}
-        max_count = 0
-        max_length= 0 
-        while end<len(s):
-            right_char = s[end]
-            if right_char not in char_count:
-                char_count[right_char] = 0 
-            char_count[right_char] += 1
-            
-            max_count = max(max_count, char_count[right_char])
-            
-            if end-start+1 <= max_count+k:
-                max_length = max(max_length, end-start + 1)
-            
-            while end-start+1 > max_count + k:
-                left_char = s[start]
-                char_count[left_char]  -= 1
-                if char_count[left_char]==0:
-                    del char_count[left_char]
-                start += 1                        
-            end+= 1
-        return max_length
-            
+        hashmap = {}
+        max_count  = 0
+        max_length = 0
         
+        while end < len(s):
+
+            right_char = s[end]
+            if right_char not in hashmap:
+                hashmap[right_char] = 0
+            hashmap[right_char] += 1
+            
+            # max_repeating char so far 
+            max_count = max(max_count, hashmap[right_char])
+            
+            if end-start+1 <= k + max_count: # ** <=
+                max_length = max(max_length, end-start+1)
+            
+            while end-start+1 > k + max_count:
+                left_char = s[start]
+                hashmap[left_char] -= 1
+                
+                if hashmap[left_char]==0:
+                    del hashmap[left_char]
+                start += 1
+            end +=1
+        
+        return max_length
+                
+                
