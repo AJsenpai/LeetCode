c3485db793f44f9e04c7a30ceb5958f82603ab2d
Ajay <ajayidc007@gmail.com>
2022-02-26 00:47:38 +0530

    Time: 84 ms (80.13%), Space: 15.7 MB (40.28%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,56 +1,43 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         '''
-        Using 2 pointers O(N) T | O(1) S        
+        Two ways to solve this problem 
+        1. using stack : O(N) T | O(N)  S     (readable)
+        2. Using 2 Pointers : O(N) T | O(1) S (confusing)
         '''
-        left,right = 0, len(height)-1
+        
+        ## using stack : 
+        # TO DO:
+        
+        ## using 2 pointers
+        
+        # two longest boundaries at any point
         leftMax,rightMax = height[0], height[-1]
+        
+        # calculate the water trap bw longest boundaries        
+        left,right = 0,len(height)-1
+        
         result = 0
         while left<=right:
             
-            if leftMax < rightMax : # min height
-                if height[left] > leftMax:
+            # left bounary is smaller than right boundary
+            if leftMax < rightMax:
+                
+                if height[left]>leftMax:
                     leftMax = height[left]
                 else:
                     result += leftMax - height[left]
+                    
                 left += 1
             
             else:
-                if height[right] > rightMax:
-                    rightMax = height[right]
-                else:
-                    result += rightMax - height[right]
-                right -= 1
-        
-        return result
-            
-                
-        
-        '''
-        using DP O(N) T | O(N) S
-        
-        max_left =  [4, 4, 4, 4, 4, 5]
-        max_right = [5, 5, 5, 5, 5, 5]
-        height =    [4, 2, 0, 3, 2, 5]
-        
-        minimumHeight = min(max_left,max_right) 
-        
-        if current_height < minHeight; 
-            result += minHeight - currentHeight                
-        '''
-#         leftMax = [0]*len(height)
-#         rightMax = [0]*len(height)
-#         result = 0
-#         for i in range(len(height)):
-#             leftMax[i] = max(leftMax[i-1], height[i])
-    
-#         for i in reversed(range(len(height)-1)): # 4-->0
-#             rightMax[i] = max(rightMax[i+1], height[i+1])
-        
-#         for i in range(len(height)):
-#             minHeight = min(leftMax[i], rightMax[i])
-#             if height[i] < minHeight:
-#                 result += minHeight - height[i]
-        
-#         return result
+                 
+                    if height[right]>rightMax:
+                        rightMax = height[right]
+                    else:
+                        result += rightMax - height[right]
+                    
+                    right -= 1        
+                    
+        return result                
                 
