cb47efa74471472d72bbcf0e93afbae926f91f39
Ajay <ajayidc007@gmail.com>
2022-02-27 00:02:54 +0530

    Time: 46 ms (54.96%), Space: 13.9 MB (60.15%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortColors(self, a: List[int]) -> None:
+        
+        low,mid,high = 0,0,len(a)-1
+        
+        while mid<=high:
+            if a[mid]==0:
+                self.swap(a,mid,low)
+                low += 1
+                mid += 1
+            
+            elif a[mid]==1:
+                mid += 1
+                
+            elif a[mid]==2:
+                self.swap(a,mid,high)
+                # since we are going left to right number at right 
+                # is not processed after swap so we cant increment mid
+                high -=1
+            
+
+        return a
+            
+    
+    def swap(self,a,i,j):
+        a[i],a[j] = a[j],a[i]
+        
