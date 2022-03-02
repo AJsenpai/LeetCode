73f84246f7988c0cc18dd7a200515150697cf420
Ajay <ajayidc007@gmail.com>
2022-03-02 17:24:17 +0530

    Time: 1604 ms (29.18%), Space: 22 MB (39.83%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def construct2DArray(self, a: List[int], m: int, n: int) -> List[List[int]]:
+        # m = row 
+        # n = col
         if len(a) != m*n:
             return
         
@@ -7,8 +9,8 @@ class Solution:
         
         idx=0
         
-        for i in range(m):
-            for j in range(n):
+        for i in range(m): # row 
+            for j in range(n): # col
                 td[i][j] = a[idx]
                 idx+=1
         return td
