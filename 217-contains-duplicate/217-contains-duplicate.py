6a62d479db3f6a548534f04fed43ea91c949e5fc
Ajay <ajayidc007@gmail.com>
2022-03-02 15:58:30 +0530

    Time: 755 ms (24.59%), Space: 26 MB (61.96%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,13 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool: 
         # using hashmap
-        # track_num = {} # num:idx
-        # for i,num in enumerate(nums):
-        #     if num in track_num:
-        #         return True
-        #     track_num[num] = i
-        # return False
+        track_num = {} # num:idx
+        for i,num in enumerate(nums):
+            if num in track_num:
+                return True
+            track_num[num] = i
+        return False
         
         # pythonic solution
-        return False if len(nums)== len(set(nums)) else True
+        # return False if len(nums)== len(set(nums)) else True
         
