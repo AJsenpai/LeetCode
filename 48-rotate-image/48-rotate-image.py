e20e3fe5386b40ad81dae73ae09ff0b75eded48a
Ajay <ajayidc007@gmail.com>
2022-03-02 23:47:44 +0530

    Time: 36 ms (88.18%), Space: 13.9 MB (63.80%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -15,7 +15,23 @@ class Solution:
         
         for sublist in matrix:
             sublist.reverse()
-        # return matrix
+        
+        
+#         # other technique - rotate single cell at at time
+#         for layer in range(0,len(matrix)//2):
+#             rotate(matrix,0,len(a)-1-layer)
+        
+#         def rotate(a,start,end):
+#             for i in range(start,end):
+#                 temp = a[start][start+i] # save top row val
+                
+#                 a[start][start+i] = a[end-i][start] # top ==> left
+#                 a[end-i][start] = a[end][end-i]     # left ==> bottom
+#                 a[end][end-i] = a[start+i][end]     # bottom ==> right
+#                 a[start+i][end] = temp              # right ==> top
+                
+        
+        
             
         
         
