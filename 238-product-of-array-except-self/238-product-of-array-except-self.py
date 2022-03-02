f46a7534402138ad6feef5c0bb6f6879413d12a8
Ajay <ajayidc007@gmail.com>
2022-03-02 18:07:22 +0530

    Time: 308 ms (56.54%), Space: 20.9 MB (95.24%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,21 +5,21 @@ class Solution:
         # using prefix and postfix product arrays 
         # to calcualte the product except itself array
         
-        n = len(nums)
-        prefix = [1]*n
-        postfix = [1]*n
-        product = [1]*n
+#         n = len(nums)
+#         prefix = [1]*n
+#         postfix = [1]*n
+#         product = [1]*n
         
-        for i in range(1,n):
-            prefix[i] = prefix[i-1]*nums[i-1]
+#         for i in range(1,n):
+#             prefix[i] = prefix[i-1]*nums[i-1]
         
-        for i in reversed(range(0,n-1)):
-            postfix[i] = postfix[i+1]* nums[i+1]
+#         for i in reversed(range(0,n-1)):
+#             postfix[i] = postfix[i+1]* nums[i+1]
         
-        for i in range(n):
-            product[i] = prefix[i]*postfix[i]
+#         for i in range(n):
+#             product[i] = prefix[i]*postfix[i]
         
-        return product
+#         return product
     
         # O(N) T | O(1) S
         # using prefix and postfix pointers instead of arrays
@@ -31,12 +31,12 @@ class Solution:
         for i in range(n):
             # first add then calculate next prefix product
             product[i] = prefix
-            prefix *= a[i]
+            prefix *= nums[i]
         
         postfix = 1
         for i in reversed(range(n)):
             product[i] *= postfix
-            postfix *= a[i]
+            postfix *= nums[i]
         
         return product
             
