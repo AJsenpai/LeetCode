47aa0de8593eb3853d810d3e7b0be86ebd8407e4
Ajay <ajayidc007@gmail.com>
2022-03-02 16:54:58 +0530

    Time: 172 ms (66.28%), Space: 15.4 MB (45.96%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -2,6 +2,7 @@ class Solution:
     def missingNumber(self, a: List[int]) -> int:
             # cyclic Sort
             i,n =0, len(a)
+            
             while i < n:
                 correct_index = a[i] # contains 0
                 if a[i]<n and a[i]!= a[correct_index]:
@@ -9,11 +10,12 @@ class Solution:
                 else:
                     i += 1
             
+            # find missing 
             for i in range(n):
                 if a[i]!= i :
                     return i
             
-            return n
+            return n  # last num is missing
     
     def swap(self,a,i,j):
         a[i],a[j] = a[j],a[i]
