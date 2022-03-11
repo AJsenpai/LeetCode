0bcf04e9686c7fd388eec3aadb24db82e9c25920
Ajay <ajayidc007@gmail.com>
2022-03-12 00:27:21 +0530

    Time: 63 ms (61.95%), Space: 13.9 MB (54.53%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -2,9 +2,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         hashmap = {'I':1,'V': 5,'X': 10 ,'L': 50,'C':100,'D':500,'M':1000}
         
-        result= 0
-        prev = None
-        
+        result= 0       
         # larger to smaller - add number
         # smaller before larger number - subtract smaller
         # Ex: IV, 1+5 = 6 but correct ans is 4. prev = 1, curr = 5
