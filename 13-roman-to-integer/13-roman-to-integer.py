d2963aafbb3e656934aede17b1d077c779837f7e
Ajay <ajayidc007@gmail.com>
2022-03-12 00:26:51 +0530

    Time: 63 ms (61.95%), Space: 14 MB (33.00%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        hashmap = {'I':1,'V': 5,'X': 10 ,'L': 50,'C':100,'D':500,'M':1000}
+        
+        result= 0
+        prev = None
+        
+        # logic: if previous number is smaller than the current 
+        # number than we need to subtract previous from current
+        # Ex: IV, 1+5 = 6 but correct ans is 4. prev = 1, curr = 5
+        # -1 + 5 = 4
+        
+        for i in range(len(s)):
+            if i+1 < len(s) and hashmap[s[i]] < hashmap[s[i+1]]:
+                result -= hashmap[s[i]]
+            else:
+                result += hashmap[s[i]]
+        return result
+                
