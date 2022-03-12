a71c9178e8ac4902287db122fd7f4373bb00c4d3
Ajay <ajayidc007@gmail.com>
2022-03-12 23:51:22 +0530

    Time: 65 ms (47.97%), Space: 14.4 MB (61.94%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,22 +1,17 @@
 class Solution:
-    def searchMatrix(self, a: List[List[int]], target: int) -> bool:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         # start from top right corner
         # we can either go left or go down
         
         # O(m+n) T | O(1) S
         row = 0
-        col = len(a[0])-1
-        while row < len(a) and col >=0:
-            if target < a[row][col]: # go left
-                col -= 1
-            elif target > a[row][col]: # go down
-                row += 1
-            else: # target == a[row][col]
+        col = len(matrix[0])-1
+        
+        while row < len(matrix) and col >=0:
+            if target < matrix[row][col]: # go left
+                col -= 1 
+            elif target > matrix[row][col]: # go down
+                row +=1 
+            else:
                 return True
         return False
-                
-                
-                
-                
-            
-        
