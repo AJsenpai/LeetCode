167298c5052bec55c5d7f866ae97ffb868d58a84
Ajay <ajayidc007@gmail.com>
2022-03-12 23:01:16 +0530

    Time: 85 ms (8.14%), Space: 14.1 MB (77.27%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -9,10 +9,10 @@ class Solution:
             
             mid = (start+end)//2
             
-            if mid> 0 and a[mid-1] > a[mid]:
+            if mid> start and a[mid-1] > a[mid]:
                 return a[mid]
             
-            elif mid< len(a)-1 and a[mid] > a[mid+1]:
+            elif mid< end and a[mid] > a[mid+1]:
                 return a[mid +1]
             
             # go to unsorted part
