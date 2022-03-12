e0f58b422a7ea748a4f892416248d13dcf589cf6
Ajay <ajayidc007@gmail.com>
2022-03-12 23:46:16 +0530

    Time: 78 ms (48.39%), Space: 14.5 MB (46.00%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -1,30 +1,31 @@
 class Solution:
     def search(self, a: List[int], target: int) -> bool:
-        # Edge case
+        # edge case
         if len(a)==1:
-             return target==a[0]
-            
-            
+            return a[0]==target
+        
         start,end = 0,len(a)-1
-        while start<= end:
-            mid = start+(end-start)//2
-            if a[mid]==target:
+        
+        while start<=end:
+            mid = (start+end)//2
+            
+            if a[mid] == target:
                 return True
             
-            if a[start]==a[mid] and a[end]==a[mid]:
-                start += 1
+            if a[start] == a[mid] and a[end]==a[mid]:
+                start+=1
                 end -= 1
             
-            elif a[start] <= a[mid]: # left is sorted
+            
+            elif a[start] <= a[mid]:
                 if target >= a[start] and target < a[mid]:
-                    end = mid - 1
+                    end = mid-1
                 else:
-                    start = mid + 1
+                    start = mid+1
             else:
                 if target <= a[end] and target > a[mid]:
-                    start = mid + 1
+                    start = mid+1
                 else:
-                    end = mid -1 
+                    end = mid-1
         return False
-
         
