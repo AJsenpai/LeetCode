8c78e45189d066662177da03b919b52181d74cd3
Ajay <ajayidc007@gmail.com>
2022-03-16 22:09:57 +0530

    Time: 45 ms (59.48%), Space: 14.3 MB (18.17%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -2,21 +2,31 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
         op = []
-        self.solve(nums,op,result)
+        # self.solve(nums,op,result)
+        self.solve(nums,0,len(nums),op,result)
         return result
     
-    def solve(self, ip, output,result):
-        if len(ip)==0:
-            result.append(output)
-            return
+    # def solve(self, ip, output,result):
+    
+    def solve(self, a, start,end,output,result):
+        result.append(output[:])
+        for i in range(start,end):
+            output.append(a[i])
+            self.solve(a,i+1,end,output,result)
+            output.pop()
         
-        output1 = output[:]
-        output2 = output[:]
         
-        output2.append(ip[0])
+#         if len(ip)==0:
+#             result.append(output)
+#             return
         
-        ip = ip[1:]
+#         output1 = output[:]
+#         output2 = output[:]
         
-        self.solve(ip,output1,result)
-        self.solve(ip,output2,result)
+#         output2.append(ip[0])
+        
+#         ip = ip[1:]
+        
+#         self.solve(ip,output1,result)
+#         self.solve(ip,output2,result)
         
