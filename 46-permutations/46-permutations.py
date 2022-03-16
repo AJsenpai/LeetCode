57ead4680911b59a483cea4ccb09541da4a3b4af
Ajay <ajayidc007@gmail.com>
2022-03-16 18:46:10 +0530

    Time: 57 ms (52.06%), Space: 14.1 MB (70.18%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,25 +1,36 @@
 class Solution:
-    def permute(self, ip: List[int]) -> List[List[int]]:
-        # decsion tree - every node gives the result
-        # as we move down pop the element from start and add it again at the end
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, end):
+            if start == end:
+                ans.append(nums[:])
+            for i in range(start, end):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start+1, end)
+                nums[start], nums[i] = nums[i], nums[start]
+                
+        ans = []
+        backtrack(0, len(nums))
+        return ans
+#         # decsion tree - every node gives the result
+#         # as we move down pop the element from start and add it again at the end
         
-        result = []
+#         result = []
         
-        # base case
-        if len(ip)==1:
-            return [ip[:]]
+#         # base case
+#         if len(ip)==1:
+#             return [ip[:]]
             
-        for  i in range(len(ip)):
-            n = ip.pop(0)
+#         for  i in range(len(ip)):
+#             n = ip.pop(0)
             
-            permutations = self.permute(ip) # recursive call
+#             permutations = self.permute(ip) # recursive call
             
-            # add back the popped number at the end
-            for p in permutations:
-                p.append(n)
+#             # add back the popped number at the end
+#             for p in permutations:
+#                 p.append(n)
             
-            # add list of permutations to result
-            result.extend(permutations)
-            ip.append(n)
-        return result
+#             # add list of permutations to result
+#             result.extend(permutations)
+#             ip.append(n)
+#         return result
                 
