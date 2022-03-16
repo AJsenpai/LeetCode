922dab4f20f1f727ead84e5e20ecd2fa4ccfbd61
Ajay <ajayidc007@gmail.com>
2022-03-17 00:29:51 +0530

    Time: 60 ms (94.56%), Space: 14 MB (40.04%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        candidates.sort(reverse=True)
+        output = []
+        self.solve(candidates,0,len(candidates),output,result,target)
+        return result
+    
+    def solve(self,a,start,end,output,result,target):
+        if target==0:
+            result.append(output[:])
+        
+        elif target>0:
+            for  i in range(start,end):
+                output.append(a[i])
+                self.solve(a,i,end,output,result,target-a[i])
+                output.pop()
+        
