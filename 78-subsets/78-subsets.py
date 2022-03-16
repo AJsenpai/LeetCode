631e4e170843eeac4972d66d87ca114b8001ac9d
Ajay <ajayidc007@gmail.com>
2022-03-16 22:10:49 +0530

    Time: 53 ms (39.37%), Space: 14.1 MB (83.26%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -2,31 +2,30 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
         op = []
-        # self.solve(nums,op,result)
-        self.solve(nums,0,len(nums),op,result)
+        self.solve(nums,op,result)        
         return result
     
-    # def solve(self, ip, output,result):
+    def solve(self, ip, output,result):           
+        if len(ip)==0:
+            result.append(output)
+            return
+        
+        output1 = output[:]
+        output2 = output[:]
+        
+        output2.append(ip[0])
+        
+        ip = ip[1:]
+        
+        self.solve(ip,output1,result)
+        self.solve(ip,output2,result)
     
-    def solve(self, a, start,end,output,result):
-        result.append(output[:])
-        for i in range(start,end):
-            output.append(a[i])
-            self.solve(a,i+1,end,output,result)
-            output.pop()
+#     def solve(self, a, start,end,output,result):
+#         result.append(output[:])
+
+#         for i in range(start,end):
+#             output.append(a[i])
+#             self.solve(a,i+1,end,output,result)
+#             output.pop()
         
-        
-#         if len(ip)==0:
-#             result.append(output)
-#             return
-        
-#         output1 = output[:]
-#         output2 = output[:]
-        
-#         output2.append(ip[0])
-        
-#         ip = ip[1:]
-        
-#         self.solve(ip,output1,result)
-#         self.solve(ip,output2,result)
         
