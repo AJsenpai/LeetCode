f3b8cb69823225342536f3f273c6d15ea91373c0
Ajay <ajayidc007@gmail.com>
2022-03-16 15:41:14 +0530

    Time: 94 ms (39.20%), Space: 14.7 MB (63.67%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,29 +1,35 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        op = ''
-        result= []
-        self.solveLCP(s,op,result)
+        output = ''
+        result = []        
+        self.solve(s,output,result)
         return result
     
-    def solveLCP(self, ip,output,result):
+    def solve(self, ip,output,result):
         if len(ip)==0:
             result.append(output)
             return
         
-        if ip[0].isdigit():
-            output1 = output[:]
-            output1 += ip[0]
-            ip = ip[1:]
-            self.solveLCP(ip,output1,result)
-        else:
+        # getting 2 choices only when input is alphabet
+        if ip[0].isalpha():
             output1 = output[:]
             output2 = output[:]
             
             output1 += ip[0].lower()
             output2 += ip[0].upper()
+            
+            ip = ip[1:]            
+            self.solve(ip,output1,result)
+            self.solve(ip,output2,result)
+        
+        else: #digit
+            output1 = output[:]
+            output1 += ip[0]
+            
             ip = ip[1:]
-            self.solveLCP(ip,output1,result)
-            self.solveLCP(ip,output2,result)
-            
-            
+            self.solve(ip,output1,result)
+    
+    
+    
+        
         
