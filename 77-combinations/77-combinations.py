e859896367699e579ba174418fbf4b4bbee905d1
Ajay <ajayidc007@gmail.com>
2022-03-17 17:07:35 +0530

    Time: 444 ms (73.91%), Space: 15.9 MB (72.54%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,47 +1,15 @@
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        sol=[]
-        
-        def backtrack(remain,comb,nex):
-			# solution found
-            if remain==0:
-                sol.append(comb[:])
-            else:
-				# iterate through all possible candidates
-                for i in range(nex,n+1):
-					# add candidate
-                    comb.append(i)
-					#backtrack
-                    backtrack(remain-1,comb,i+1)
-					# remove candidate
-                    comb.pop()
-            
-        backtrack(k,[],1)
-        return sol
-#         if not n or k<=0:
-#             return []
-        
-#         buffer = [None] * k
-#         result = []
-#         a = [i for i in range(1,n+1)]
-        
-#         self.solve(a,buffer,0,0,result)
-#         return result
+    def combine(self, n: int, size: int) -> List[List[int]]:
+        result = []
+        output = []
+        self.solve(1,n+1,output,result,size)
+        return result
     
-#     def solve(self,a,buffer,bufferIdx,startIdx,result):                
-#         if bufferIdx == len(buffer):            
-#             print(buffer)
-#             # result.append(buffer)
-#             return 
-        
-        
-#         if startIdx==len(a):
-#             return 
-        
-#         for i in range(startIdx,len(a)):            
-#             buffer[bufferIdx] = a[i]
-#             self.solve(a,buffer,bufferIdx+1,i+1,result)
-        
-        
-        
-        
+    def solve(self,start,end,output,result,size):
+        if size ==0:
+            result.append(output[:])
+        else:
+            for i in range(start,end):
+                output.append(i)
+                self.solve(i+1,end,output,result,size-1)
+                output.pop()
