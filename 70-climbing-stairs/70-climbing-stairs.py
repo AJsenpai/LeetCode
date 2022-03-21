3c2a03a08bf0436100c577cc9e2f3512d121685b
Ajay <ajayidc007@gmail.com>
2022-03-21 15:33:08 +0530

    Time: 47 ms (37.82%), Space: 13.9 MB (26.88%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n ==0:
+            return 0
+        if n==1:
+            return 1
+        if n == 2:
+            return 2
+        
+        # pattern - fihonacci
+        
+        fib1,fib2 = 1,2
+        for i in range(2,n):
+            fib1,fib2 = fib2,fib1+fib2
+        return fib2
