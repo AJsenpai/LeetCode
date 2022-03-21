911705361cdf43304b28e082bc334fc8c7d288fc
Ajay <ajayidc007@gmail.com>
2022-03-21 16:45:58 +0530

    Time: 43 ms (57.73%), Space: 13.9 MB (34.16%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, a: List[int]) -> int:
+        fib1,fib2 = 0,0
+        
+        for n in a:
+            total = max(n+fib1,fib2)
+            fib1 = fib2
+            fib2 = total
+        return total
+        
+
+            
+
+            
+            
+                
+            
