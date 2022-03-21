2515de89356344323440140a78fbf62b25377358
Ajay <ajayidc007@gmail.com>
2022-03-21 17:56:26 +0530

    Time: 735 ms (41.47%), Space: 15.4 MB (23.20%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,17 +1,27 @@
 class Solution:
     def canJump(self, a: List[int]) -> bool:
-        if len(a)==1:
-            return True
+        max_position,n = 0,len(a)
         
-        max_position = 0
-        i = 0
-        
-        
-        while i<=max_position:
-            max_position = max(max_position, i + a[i])
-            if max_position >= len(a)-1:
+        for i,num in enumerate(a):
+            if max_position < i:
+                return False
+            if max_position >= n-1:
                 return True
-            i +=1
+            max_position = max(max_position, i + num)
         return False
+    
+#         if len(a)==1:
+#             return True
+        
+#         max_position = 0
+#         i = 0
+        
+        
+#         while i<=max_position:
+#             max_position = max(max_position, i + a[i])
+#             if max_position >= len(a)-1:
+#                 return True
+#             i +=1
+#         return False
         
         
