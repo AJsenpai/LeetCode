26cda71b0a892d6bbda679e902d32ad0d51a186d
Ajay <ajayidc007@gmail.com>
2022-03-21 17:20:54 +0530

    Time: 40 ms (65.28%), Space: 13.8 MB (98.15%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,13 +1,21 @@
 class Solution:
     def rob(self, a: List[int]) -> int:
-        # fibonacci,,
-        fib1,fib2 = 0,0
+        n = len(a)
+        t = [0]* (n+1)
+        t[1] = a[0]
         
-        for n in a:
-            total = max(n+fib1,fib2)
-            fib1 = fib2
-            fib2 = total
-        return total
+        for i in range(1,n):
+            t[i+1] = max(t[i-1]+a[i], t[i])
+        return t[-1]
+        
+#         # fibonacci,,
+#         fib1,fib2 = 0,0
+        
+#         for n in a:
+#             total = max(n+fib1,fib2)
+#             fib1 = fib2
+#             fib2 = total
+#         return total
         
 
             
