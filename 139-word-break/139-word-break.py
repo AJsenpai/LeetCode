0d4145edd684022a69f38a225759c3235afde9ea
Ajay <ajayidc007@gmail.com>
2022-03-22 16:43:16 +0530

    Time: 53 ms (58.59%), Space: 14.1 MB (54.07%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,5 +1,9 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # start from last and check if the word at index i
+        # is matching length of word from wordDic is matching 
+        # the current word or not
+        # ex leetcode , i = 0 , word = 'leet' s[0:0+4] = leet --> matching
         t = [False] * (len(s)+1)
         t[len(s)] = True
         
@@ -9,7 +13,7 @@ class Solution:
                     t[i] = t[i+len(word)]
                 if t[i]:
                     break
-        print(t)
+
         return t[0]
         
         
