1e541c8d0b9820316b789cd72f7b3096edc280e7
Ajay <ajayidc007@gmail.com>
2022-03-23 10:06:22 +0530

    Time: 49 ms (44.99%), Space: 13.9 MB (18.58%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -2,6 +2,6 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
         for i in range(32):
-            result = (result<<1) + (n&1)
+            result = (result<<1) | (n&1)
             n >>=1
         return result
