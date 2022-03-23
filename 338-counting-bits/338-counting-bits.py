4cf89c7facfa5ba5fd9d0fedc3e70ff173a15fbd
Ajay <ajayidc007@gmail.com>
2022-03-23 08:32:54 +0530

    Time: 143 ms (41.63%), Space: 20.9 MB (34.77%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -10,7 +10,7 @@ class Solution:
         count = 0
         while n>0:
             count += 1
-            n = n & (n-1)
+            n = n & (n-1) #v.imp
         return count
             
         
