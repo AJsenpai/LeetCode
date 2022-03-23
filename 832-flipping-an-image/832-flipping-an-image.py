279234f03468657597f96873981b406d1bc69683
Ajay <ajayidc007@gmail.com>
2022-03-23 09:39:19 +0530

    Time: 64 ms (65.65%), Space: 13.9 MB (71.79%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -1,6 +1,20 @@
 class Solution:
     def flipAndInvertImage(self, a: List[List[int]]) -> List[List[int]]:
         
+        """
+        Bitwise XOR --> 0^1 = 1, 1^1 =0 (invert)
+
+        After reviewing some examples you will notice the following patterns:
+        1.  Look at first and last value of row. If they are the same (1,1 or 0,0), 
+            they will be flipped in the output. If they are different (1,0 or 0,1), 
+            they do not change. Work your way inward to the middle of the list 
+            applying this rule.
+
+        2.  If the row has an odd number of entries, the middle value always flips. 
+            For example if len(row) = 5, then row[2] must change values.
+
+        """
+
         for row in a:
             start,end = 0, len(row)-1
             while start<=end:
