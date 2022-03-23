5f7e0b0fb3c82c17e793257427c16de7a821b585
Ajay <ajayidc007@gmail.com>
2022-03-23 09:33:29 +0530

    Time: 88 ms (25.66%), Space: 13.8 MB (97.45%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -1,15 +1,28 @@
 class Solution:
-    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        for sublist in image:
-            # flip horizontally - swap
-            sublist.reverse()
-            # invert - compliment
-            self.invert(sublist)            
-        return image
+    def flipAndInvertImage(self, a: List[List[int]]) -> List[List[int]]:
+        
+        for row in a:
+            start,end = 0, len(row)-1
+            while start<=end:
+                if row[start] == row[end]:
+                    row[start], row[end] = row[end]^1, row[start]^1
+                start += 1
+                end -=1
+        return a
+            
+        
+        
+        
+#         for sublist in image:
+#             # flip horizontally - swap
+#             sublist.reverse()
+#             # invert - compliment
+#             self.invert(sublist)            
+#         return image
     
-    def invert(self,a):
-        for i,num in enumerate(a):
-            num ^= 1 #xor
-            a[i] = num
+#     def invert(self,a):
+#         for i,num in enumerate(a):
+#             num ^= 1 #xor
+#             a[i] = num
     
         
