b863fea0dd294b48fa9281cccbb689fdef0f5d7a
Ajay <ajayidc007@gmail.com>
2022-03-27 22:49:18 +0530

    Time: 44 ms (93.75%), Space: 16.3 MB (46.72%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -15,14 +15,14 @@ class Solution:
             left = getDiameter(root.left)
             right = getDiameter(root.right)
             
-            temp = max(left,right) + 1 
-            ans = max(temp, 1+left+right)
+            temp = max(left,right) + 1  # if current node is not ans
+            ans = max(temp, 1+left+right) # if current node is ans
             
-            self.result = max(self.result, ans)
+            self.result = max(self.result, ans) 
             return temp
         
         getDiameter(root)
-        return self.result-1 # edges
+        return self.result-1 # asking for edges
         
         
         
