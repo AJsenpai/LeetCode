e97b1a15f54f5d660d881e4c9b4dc50a407445dd
Ajay <ajayidc007@gmail.com>
2022-03-30 23:56:05 +0530

    Time: 44 ms (63.87%), Space: 14 MB (24.22%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -8,27 +8,41 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return True
+        
+        return self.solve(root.left,root.right)
     
+    def solve(self,left,right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        
+        if left.val ==right.val:
+            outPair = self.solve(left.left, right.right)
+            inPair = self.solve(left.right, right.left)
+            return outPair and inPair
+        else:
+            return False
     
-        # mimicing recursion call stack
-        queue = deque()
-        queue.append([root.left, root.right])
+        # mimicing recursion call stack - iterative
+#         queue = deque()
+#         queue.append([root.left, root.right])
 
-        while queue:
-            pair = queue.popleft()
-            left,right = pair[0],pair[1]
+#         while queue:
+#             pair = queue.popleft()
+#             left,right = pair[0],pair[1]
 
-            if left is None and right is None:
-                continue
-            if left is None or right is None:
-                return False
+#             if left is None and right is None:
+#                 continue
+#             if left is None or right is None:
+#                 return False
 
-            if left.val==right.val:
-                queue.appendleft([left.left,right.right]) # outward
-                queue.appendleft([left.right,right.left]) # inward
-            else:
-                return False
-        return True
+#             if left.val==right.val:
+#                 queue.appendleft([left.left,right.right]) # outward
+#                 queue.appendleft([left.right,right.left]) # inward
+#             else:
+#                 return False
+#         return True
         
 
         
