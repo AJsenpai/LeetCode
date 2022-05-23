0adf8806a80dc80e89e5a2dfcc13f87e72a5b1a3
Ajay <ajayidc007@gmail.com>
2022-05-23 16:08:06 +0530

    Time: 94 ms (82.95%), Space: 14.7 MB (80.87%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -1,19 +1,24 @@
 class Solution:
     def matrixReshape(self, m: List[List[int]], r: int, c: int) -> List[List[int]]:
         
-        # O(m*n) T | O(m*n) S
+        # O(m*n) T | O(m*n) S        
+#         m_row,m_col = len(m), len(m[0])
         
-        m_row,m_col = len(m), len(m[0])
+#         if m_row*m_col!=r*c:
+#             return m
         
-        if m_row*m_col!=r*c:
+#         reshaped = [[]]
+        
+#         for i in range(m_row):
+#             for j in range(m_col):
+#                 if len(reshaped[-1])==c:
+#                     reshaped.append([])
+#                 reshaped[-1].append(m[i][j])
+#         return reshaped
+        flat = sum(m, [])
+        if len(flat) != r * c:
             return m
+        tuples = zip(*([iter(flat)] * c))
+        return map(list, tuples)
         
-        reshaped = [[]]
-        
-        for i in range(m_row):
-            for j in range(m_col):
-                if len(reshaped[-1])==c:
-                    reshaped.append([])
-                reshaped[-1].append(m[i][j])
-        return reshaped
         
