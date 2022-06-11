8dc4db6d131c07439c2871bce9d90de00f7f1285
Ajay <ajayidc007@gmail.com>
2022-06-11 14:01:42 +0530

    Time: 531 ms (29.92%), Space: 25 MB (29.15%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minDifference(self, a: List[int]) -> int:
+        a.sort()
+        
+        result = float('inf')
+        
+        n = len(a)
+        i = 0 
+        j = n-1-3  # N - 1 is the last element and we want to exlude 3 elements
+        
+        while i<=j and j<n:
+            result = min(result, a[j]-a[i]) 
+            i+=1 
+            j+=1
+        return result if result != float('inf') else 0
+        
