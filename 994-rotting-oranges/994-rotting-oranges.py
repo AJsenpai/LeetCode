f73d869186521a48b7b669e1855b13bafcfce25d
Ajay <ajayidc007@gmail.com>
2022-06-16 19:51:22 +0530

    Time: 123 ms (5.16%), Space: 14 MB (46.18%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,45 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # O(mn) T | O(mn) S
+        rows,cols = len(grid),len(grid[0])
+        fresh,rotten = set(),set()
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==1:
+                    fresh.add((r,c))
+                if grid[r][c]==2:
+                    rotten.add((r,c))
+        
+        mins = 0
+        deltas = [(0,1),(0,-1),(1,0),(-1,0)]
+        while fresh:
+            mins +=1
+            new_rotten = set()
+            
+            for r,c in rotten:
+                for dr,dc in deltas:
+                    if (r+dr,c+dc) in fresh:
+                        new_rotten.add((r+dr,c+dc))
+            
+            if not new_rotten:
+                return -1
+            rotten = new_rotten
+            fresh -= new_rotten # set operation (difference)
+        return mins
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+                
+                        
+            
