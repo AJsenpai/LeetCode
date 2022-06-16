09e59ab4d640308ea258c4467a40a44b5dc4b59c
Ajay <ajayidc007@gmail.com>
2022-06-16 19:42:19 +0530

    Time: 83 ms (76.34%), Space: 14.9 MB (59.18%) - LeetHub

---
+++ 120-triangle/120-triangle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # dynamic programming - bottom up
+        
+        if not triangle:
+            return 
+        
+        # traversing from second last row of triangle to top
+        for row in range(len(triangle)-2,-1,-1):
+            for col in range(len(triangle[row])): # **
+                
+                # compare value with its bottom value of left and right
+                # triangle = [[11], [9, 10], [7, 6, 10], [4, 1, 8, 3]]
+                triangle[row][col] += min(triangle[row+1][col],
+                                         triangle[row+1][col+1])
+        
+        return triangle[0][0]
+        
