141c739029a6b79ded25f92752693402da8a6974
Ajay <ajayidc007@gmail.com>
2022-06-18 11:34:38 +0530

    Time: 81 ms (42.19%), Space: 13.9 MB (46.48%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,32 +1,33 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         # O(mn) T | O(mn) S
-        rows,cols = len(grid),len(grid[0])
-        fresh,rotten = set(),set()
+        ROWS,COLS = len(grid),len(grid[0])
+        fresh,queue = 0, deque() # fresh, rotten-->queue
         
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c]==1:
-                    fresh.add((r,c))
+                    fresh+=1
                 if grid[r][c]==2:
-                    rotten.add((r,c))
+                    queue.append((r,c))
         
         mins = 0
-        deltas = [(0,1),(0,-1),(1,0),(-1,0)] # 4 adjacent directions we can move in
-        while fresh:
+        directions = [(0,1),(0,-1),(1,0),(-1,0)] # 4 adjacent directions we can move in
+        while fresh and queue:
+            
+            for _ in range(len(queue)):
+                r,c = queue.popleft()            
+                
+                for dr,dc in directions:
+                    row,col = dr+r, dc+c
+                    if (0<=row<ROWS and 0<=col<COLS and grid[row][col]==1):
+                        grid[row][col]=2
+                        queue.append((row,col))
+                        fresh -=1
+                
             mins +=1
-            new_rotten = set()
-            
-            for r,c in rotten:
-                for dr,dc in deltas:
-                    if (r+dr,c+dc) in fresh:
-                        new_rotten.add((r+dr,c+dc))
-            
-            if not new_rotten:
-                return -1
-            rotten = new_rotten
-            fresh -= new_rotten # set operation (difference)
-        return mins
+                        
+        return mins if fresh==0 else -1
             
             
             
