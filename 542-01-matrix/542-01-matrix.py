0a6234dfd853e9f6dca9137c9b35fdf8a34863ee
Ajay <ajayidc007@gmail.com>
2022-06-18 11:36:20 +0530

    Time: 1347 ms (14.07%), Space: 17 MB (70.80%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -2,7 +2,7 @@ class Solution:
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
         # BFS
         rows,cols = len(matrix),len(matrix[0])
-        deltas = [(1,0),(-1,0),(0,1),(0,-1)]
+        deltas = [(1,0),(-1,0),(0,1),(0,-1)] # 4 adjacent directions we can move in
             
         queue = deque()            
         max_dist = max(rows,cols)
