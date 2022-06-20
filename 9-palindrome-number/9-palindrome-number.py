99f2f66727940a150878171e0bf8651ebc61a0d3
Ajay <ajayidc007@gmail.com>
2022-06-20 15:53:54 +0530

    Time: 140 ms (9.97%), Space: 13.9 MB (16.18%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -5,11 +5,22 @@ class Solution:
         # return str(s) == str(s)[::-1]
         
         # solution 2
-        s = str(s)
-        left,right = 0, len(s)-1
-        while left<right:
-            if s[left]!=s[right]:
-                return False
-            left +=1
-            right -=1
-        return True
+        # s = str(s)
+        # left,right = 0, len(s)-1
+        # while left<right:
+        #     if s[left]!=s[right]:
+        #         return False
+        #     left +=1
+        #     right -=1
+        # return True
+    
+        # solution3 
+        new_num = 0 
+        x = s
+        while x>0:
+            new_num = (new_num*10)+ (x%10)
+            x = x//10
+        print(new_num,s)
+        return new_num == s
+    
+   
