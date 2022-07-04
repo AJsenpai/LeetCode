df8632f7b0f1dc7aaffb131f96ffd047805490ff
Ajay <ajayidc007@gmail.com>
2022-07-04 16:18:59 +0530

    Time: 100 ms (11.92%), Space: 14.3 MB (6.39%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -1,26 +1,43 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         
-        
-        # returns the next sequence
-        # example s=21 --> 1211
-        def next_number(s):
-            
-            result = []
-            i=0
-        
-            while i <len(s):
-                count = 1
-                while i+1 < len(s) and s[i]==s[i+1]:
-                    count+=1
-                    i+=1
-                result.append(str(count)+s[i])
-                i+=1
-            
-            return ''.join(result)
-        
-        s = "1"
+        # Solution 2
+        sequence = [1]
         for _ in range(n-1):
-            s = next_number(s)
-        return s
+            next_seq = []
+            for num in sequence:
+                if not next_seq or next_seq[-1]!=num:
+                    next_seq += [1, num]
+                else:
+                    next_seq[-2] +=1
+            sequence = next_seq
+        
+        return ''.join(map(str, sequence))
+            
+
+        
+        
+        # Solution 1
+        
+#         # returns the next sequence
+#         # example s=21 --> 1211
+#         def next_number(s):
+            
+#             result = []
+#             i=0
+        
+#             while i <len(s):
+#                 count = 1
+#                 while i+1 < len(s) and s[i]==s[i+1]:
+#                     count+=1
+#                     i+=1
+#                 result.append(str(count)+s[i])
+#                 i+=1
+            
+#             return ''.join(result)
+        
+#         s = "1"
+#         for _ in range(n-1):
+#             s = next_number(s)
+#         return s
             
